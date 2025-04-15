bd8f7d9339519c34d8b97da0833d654d2c55896c
peduu87 <pepe.silva1202@gmail.com>
2025-04-15 15:37:02 -0300

    Early stage fetch

--- scriptMain.js
+++ scriptMain.js
@@ -1,3 +1,22 @@
 function searchProfile() {
-    
+    const username = document.getElementById("searchBar").value;
+    const errorArea = document.getElementById("errorArea");
+    const url = `https://api.github.com/users/${username}`;
+
+    if (username.replaceAll(" ", "") == "") {
+        errorArea.innerHTML = `<div class="alert alert-danger" role="alert">
+            Invalid search. <i>Null</i> values can't be found.
+        </div>`;
+
+        return;
+    }
+    else {
+        errorArea.innerHTML = "";
+    }
+
+    let profileJson = fetch(url).then( (response) => {
+        return response.json();
+    });
+
+    console.log(profileJson.id);
 }
